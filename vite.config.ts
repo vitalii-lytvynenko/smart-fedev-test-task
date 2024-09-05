d8056befc0ad69ba8914e8ffd4ac94357be0bbdf
vitalii-lytvynenko <reskator95@gmail.com>
2024-09-06 00:33:42 +0300

    deploy task

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: 'https://vitalii-lytvynenko.github.io/smart-fedev-test-task',
+});
+
+
