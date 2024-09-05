e0fe3aa04a85a0a7ef1b62e32bccfd76a2015ae3
vitalii-lytvynenko <reskator95@gmail.com>
2024-09-05 16:45:47 +0300

    initial commit

---
+++ eslint.config.js
@@ -0,0 +1,29 @@
+import js from '@eslint/js'
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
+
+export default tseslint.config(
+  { ignores: ['dist'] },
+  {
+    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+    plugins: [
+      'react',
+      'react-hooks',
+      'react-refresh',
+      '@typescript-eslint', 
+      'prettier'
+    ],
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+    },
+  },
+)
---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className='bg-amber-500'>
+        Test
+      </div>
+    </>
+  )
+}
+
+export default App
